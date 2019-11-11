3d90aad10ec26119cea61ea7f3d2e84e30396cd5
Mishrule <mishrule1@gmail.com>
2019-11-11 14:08:35 +0000

    library and prep prefect result created

--- phpScript/displayVoteResults.php
+++ phpScript/displayVoteResults.php
@@ -413,4 +413,214 @@
         }
     }
 
+     /*
+    *==============================================================================================
+    *                               PREP PREFECT RESULT
+    *==============================================================================================
+    */
+     //==============================| ZERO HEALTH PREFECT
+    // if(isset($_POST['dinning_prefectZero'])){
+    //     $dinning_PrefectZero = mysqli_real_escape_string($conn, $_POST['dinning_prefectZero']);
+    //     $dinning_PrefectZeroSQL = "SELECT COUNT(dinning_hall_prefect) AS dinning_prefectZero FROM vote WHERE dinning_hall_prefect = '$dinning_PrefectZero'";
+    //     $dinning_PrefectZeroRESULT = mysqli_query($conn, $dinning_PrefectZeroSQL);
+    //     while($dinning_PrefectZeroROW = mysqli_fetch_array($dinning_PrefectZeroRESULT)){
+    //         echo $dinning_PrefectZeroROW['dinning_prefectZero'].' <h6>VOTE(S)</h6>';
+    //     }
+    // }
+     //==============================| FIRST prep PREFECT
+    if(isset($_POST['prep_prefectOne'])){
+        $prep_PrefectOne = mysqli_real_escape_string($conn, $_POST['prep_prefectOne']);
+        $prep_PrefectOneSQL = "SELECT COUNT(prep_prefect) AS prep_prefectone FROM vote WHERE prep_prefect = '$prep_PrefectOne'";
+        $prep_PrefectOneRESULT = mysqli_query($conn, $prep_PrefectOneSQL);
+        while($prep_PrefectOneROW = mysqli_fetch_array($prep_PrefectOneRESULT)){
+            echo $prep_PrefectOneROW['prep_prefectone'].' <h6>VOTE(S)</h6>';
+        }
+    }
+        //==============================| SECOND prep PREFECT
+    if(isset($_POST['prep_prefectTwo'])){
+        $prep_PrefectTwo = mysqli_real_escape_string($conn, $_POST['prep_prefectTwo']);
+        $prep_PrefectTwoSQL = "SELECT COUNT(prep_prefect) AS prep_prefectTwo FROM vote WHERE prep_prefect = '$prep_PrefectTwo'";
+        $prep_PrefectTwoRESULT = mysqli_query($conn, $prep_PrefectTwoSQL);
+        while($prep_PrefectTwoROW = mysqli_fetch_array($prep_PrefectTwoRESULT)){
+            echo $prep_PrefectTwoROW['prep_prefectTwo'].' <h6>VOTE(S)</h6>';
+        }
+    }
+       //==============================| THIRD prep PREFECT
+    if(isset($_POST['prep_prefectThree'])){
+        $prep_PrefectThree = mysqli_real_escape_string($conn, $_POST['prep_prefectThree']);
+        $prep_PrefectThreeSQL = "SELECT COUNT(prep_prefect) AS prep_prefectThree FROM vote WHERE prep_prefect = '$prep_PrefectThree'";
+        $prep_PrefectThreeRESULT = mysqli_query($conn, $prep_PrefectThreeSQL);
+        while($prep_PrefectThreeROW = mysqli_fetch_array($prep_PrefectThreeRESULT)){
+            echo $prep_PrefectThreeROW['prep_prefectThree'].' <h6>VOTE(S)</h6>';
+        }
+    }
+       //==============================| FOURTH prep PREFECT
+    if(isset($_POST['prep_prefectFour'])){
+        $prep_PrefectFour = mysqli_real_escape_string($conn, $_POST['prep_prefectFour']);
+        $prep_PrefectFourSQL = "SELECT COUNT(prep_prefect) AS prep_prefectFour FROM vote WHERE prep_prefect = '$prep_PrefectFour'";
+        $prep_PrefectFourRESULT = mysqli_query($conn, $prep_PrefectFourSQL);
+        while($prep_PrefectFourROW = mysqli_fetch_array($prep_PrefectFourRESULT)){
+            echo $prep_PrefectFourROW['prep_prefectFour'].' <h6>VOTE(S)</h6>';
+        }
+    }
+       //==============================| FIFTH prep PREFECT
+    if(isset($_POST['prep_prefectFive'])){
+        $prep_PrefectFive = mysqli_real_escape_string($conn, $_POST['prep_prefectFive']);
+        $prep_PrefectFiveSQL = "SELECT COUNT(prep_prefect) AS prep_prefectFive FROM vote WHERE prep_prefect = '$prep_PrefectFive'";
+        $prep_PrefectFiveRESULT = mysqli_query($conn, $prep_PrefectFiveSQL);
+        while($prep_PrefectFiveROW = mysqli_fetch_array($prep_PrefectFiveRESULT)){
+            echo $prep_PrefectFiveROW['prep_prefectFive'].' <h6>VOTE(S)</h6>';
+        }
+    }
+       //==============================| SIXTH prep PREFECT
+    if(isset($_POST['prep_prefectSix'])){
+        $prep_PrefectSix = mysqli_real_escape_string($conn, $_POST['prep_prefectSix']);
+        $prep_PrefectSixSQL = "SELECT COUNT(prep_prefect) AS prep_prefectSix FROM vote WHERE prep_prefect = '$prep_PrefectSix'";
+        $prep_PrefectSixRESULT = mysqli_query($conn, $prep_PrefectSixSQL);
+        while($prep_PrefectSixROW = mysqli_fetch_array($prep_PrefectSixRESULT)){
+            echo $prep_PrefectSixROW['prep_prefectSix'].' <h6>VOTE(S)</h6>';
+        }
+    }
+       //==============================| SEVEN prep PREFECT
+    if(isset($_POST['prep_prefectSeven'])){
+        $prep_PrefectSeven = mysqli_real_escape_string($conn, $_POST['prep_prefectSeven']);
+        $prep_PrefectSevenSQL = "SELECT COUNT(prep_prefect) AS prep_prefectSeven FROM vote WHERE prep_prefect = '$prep_PrefectSeven'";
+        $prep_PrefectSevenRESULT = mysqli_query($conn, $prep_PrefectSevenSQL);
+        while($prep_PrefectSevenROW = mysqli_fetch_array($prep_PrefectSevenRESULT)){
+            echo $prep_PrefectSevenROW['prep_prefectSeven'].' <h6>VOTE(S)</h6>';
+        }
+    }
+       //==============================| EIGHT prep PREFECT
+    if(isset($_POST['prep_prefectEight'])){
+        $prep_PrefectEight = mysqli_real_escape_string($conn, $_POST['prep_prefectEight']);
+        $prep_PrefectEightSQL = "SELECT COUNT(prep_prefect) AS prep_prefectEight FROM vote WHERE prep_prefect = '$prep_PrefectEight'";
+        $prep_PrefectEightRESULT = mysqli_query($conn, $prep_PrefectEightSQL);
+        while($prep_PrefectEightROW = mysqli_fetch_array($prep_PrefectEightRESULT)){
+            echo $prep_PrefectEightROW['prep_prefectEight'].' <h6>VOTE(S)</h6>';
+        }
+    }
+       //==============================| NINE prep PREFECT
+    if(isset($_POST['prep_prefectNine'])){
+        $prep_PrefectNine = mysqli_real_escape_string($conn, $_POST['prep_prefectNine']);
+        $prep_PrefectNineSQL = "SELECT COUNT(prep_prefect) AS prep_prefectNine FROM vote WHERE prep_prefect = '$prep_PrefectNine'";
+        $prep_PrefectNineRESULT = mysqli_query($conn, $prep_PrefectNineSQL);
+        while($prep_PrefectNineROW = mysqli_fetch_array($prep_PrefectNineRESULT)){
+            echo $prep_PrefectNineROW['prep_prefectNine'].' <h6>VOTE(S)</h6>';
+        }
+    }
+       //==============================| TEN prep PREFECT
+    if(isset($_POST['prep_prefectTen'])){
+        $prep_PrefectTen = mysqli_real_escape_string($conn, $_POST['prep_prefectTen']);
+        $prep_PrefectTenSQL = "SELECT COUNT(prep_prefect) AS prep_prefectTen FROM vote WHERE prep_prefect = '$prep_PrefectTen'";
+        $prep_PrefectTenRESULT = mysqli_query($conn, $prep_PrefectTenSQL);
+        while($prep_PrefectTenROW = mysqli_fetch_array($prep_PrefectTenRESULT)){
+            echo $prep_PrefectTenROW['prep_prefectTen'].' <h6>VOTE(S)</h6>';
+        }
+    }
+
+      /*
+    *==============================================================================================
+    *                               LIBRARY PREFECT RESULT
+    *==============================================================================================
+    */
+     //==============================| ZERO HEALTH PREFECT
+    // if(isset($_POST['dinning_prefectZero'])){
+    //     $dinning_PrefectZero = mysqli_real_escape_string($conn, $_POST['dinning_prefectZero']);
+    //     $dinning_PrefectZeroSQL = "SELECT COUNT(dinning_hall_prefect) AS dinning_prefectZero FROM vote WHERE dinning_hall_prefect = '$dinning_PrefectZero'";
+    //     $dinning_PrefectZeroRESULT = mysqli_query($conn, $dinning_PrefectZeroSQL);
+    //     while($dinning_PrefectZeroROW = mysqli_fetch_array($dinning_PrefectZeroRESULT)){
+    //         echo $dinning_PrefectZeroROW['dinning_prefectZero'].' <h6>VOTE(S)</h6>';
+    //     }
+    // }
+     //==============================| FIRST library PREFECT
+    if(isset($_POST['library_prefectOne'])){
+        $library_PrefectOne = mysqli_real_escape_string($conn, $_POST['library_prefectOne']);
+        $library_PrefectOneSQL = "SELECT COUNT(library_prefect) AS library_prefectone FROM vote WHERE library_prefect = '$library_PrefectOne'";
+        $library_PrefectOneRESULT = mysqli_query($conn, $library_PrefectOneSQL);
+        while($library_PrefectOneROW = mysqli_fetch_array($library_PrefectOneRESULT)){
+            echo $library_PrefectOneROW['library_prefectone'].' <h6>VOTE(S)</h6>';
+        }
+    }
+        //==============================| SECOND library PREFECT
+    if(isset($_POST['library_prefectTwo'])){
+        $library_PrefectTwo = mysqli_real_escape_string($conn, $_POST['library_prefectTwo']);
+        $library_PrefectTwoSQL = "SELECT COUNT(library_prefect) AS library_prefectTwo FROM vote WHERE library_prefect = '$library_PrefectTwo'";
+        $library_PrefectTwoRESULT = mysqli_query($conn, $library_PrefectTwoSQL);
+        while($library_PrefectTwoROW = mysqli_fetch_array($library_PrefectTwoRESULT)){
+            echo $library_PrefectTwoROW['library_prefectTwo'].' <h6>VOTE(S)</h6>';
+        }
+    }
+       //==============================| THIRD library PREFECT
+    if(isset($_POST['library_prefectThree'])){
+        $library_PrefectThree = mysqli_real_escape_string($conn, $_POST['library_prefectThree']);
+        $library_PrefectThreeSQL = "SELECT COUNT(library_prefect) AS library_prefectThree FROM vote WHERE library_prefect = '$library_PrefectThree'";
+        $library_PrefectThreeRESULT = mysqli_query($conn, $library_PrefectThreeSQL);
+        while($library_PrefectThreeROW = mysqli_fetch_array($library_PrefectThreeRESULT)){
+            echo $library_PrefectThreeROW['library_prefectThree'].' <h6>VOTE(S)</h6>';
+        }
+    }
+       //==============================| FOURTH library PREFECT
+    if(isset($_POST['library_prefectFour'])){
+        $library_PrefectFour = mysqli_real_escape_string($conn, $_POST['library_prefectFour']);
+        $library_PrefectFourSQL = "SELECT COUNT(library_prefect) AS library_prefectFour FROM vote WHERE library_prefect = '$library_PrefectFour'";
+        $library_PrefectFourRESULT = mysqli_query($conn, $library_PrefectFourSQL);
+        while($library_PrefectFourROW = mysqli_fetch_array($library_PrefectFourRESULT)){
+            echo $library_PrefectFourROW['library_prefectFour'].' <h6>VOTE(S)</h6>';
+        }
+    }
+       //==============================| FIFTH library PREFECT
+    if(isset($_POST['library_prefectFive'])){
+        $library_PrefectFive = mysqli_real_escape_string($conn, $_POST['library_prefectFive']);
+        $library_PrefectFiveSQL = "SELECT COUNT(library_prefect) AS library_prefectFive FROM vote WHERE library_prefect = '$library_PrefectFive'";
+        $library_PrefectFiveRESULT = mysqli_query($conn, $library_PrefectFiveSQL);
+        while($library_PrefectFiveROW = mysqli_fetch_array($library_PrefectFiveRESULT)){
+            echo $library_PrefectFiveROW['library_prefectFive'].' <h6>VOTE(S)</h6>';
+        }
+    }
+       //==============================| SIXTH library PREFECT
+    if(isset($_POST['library_prefectSix'])){
+        $library_PrefectSix = mysqli_real_escape_string($conn, $_POST['library_prefectSix']);
+        $library_PrefectSixSQL = "SELECT COUNT(library_prefect) AS library_prefectSix FROM vote WHERE library_prefect = '$library_PrefectSix'";
+        $library_PrefectSixRESULT = mysqli_query($conn, $library_PrefectSixSQL);
+        while($library_PrefectSixROW = mysqli_fetch_array($library_PrefectSixRESULT)){
+            echo $library_PrefectSixROW['library_prefectSix'].' <h6>VOTE(S)</h6>';
+        }
+    }
+       //==============================| SEVEN library PREFECT
+    if(isset($_POST['library_prefectSeven'])){
+        $library_PrefectSeven = mysqli_real_escape_string($conn, $_POST['library_prefectSeven']);
+        $library_PrefectSevenSQL = "SELECT COUNT(library_prefect) AS library_prefectSeven FROM vote WHERE library_prefect = '$library_PrefectSeven'";
+        $library_PrefectSevenRESULT = mysqli_query($conn, $library_PrefectSevenSQL);
+        while($library_PrefectSevenROW = mysqli_fetch_array($library_PrefectSevenRESULT)){
+            echo $library_PrefectSevenROW['library_prefectSeven'].' <h6>VOTE(S)</h6>';
+        }
+    }
+       //==============================| EIGHT library PREFECT
+    if(isset($_POST['library_prefectEight'])){
+        $library_PrefectEight = mysqli_real_escape_string($conn, $_POST['library_prefectEight']);
+        $library_PrefectEightSQL = "SELECT COUNT(library_prefect) AS library_prefectEight FROM vote WHERE library_prefect = '$library_PrefectEight'";
+        $library_PrefectEightRESULT = mysqli_query($conn, $library_PrefectEightSQL);
+        while($library_PrefectEightROW = mysqli_fetch_array($library_PrefectEightRESULT)){
+            echo $library_PrefectEightROW['library_prefectEight'].' <h6>VOTE(S)</h6>';
+        }
+    }
+       //==============================| NINE library PREFECT
+    if(isset($_POST['library_prefectNine'])){
+        $library_PrefectNine = mysqli_real_escape_string($conn, $_POST['library_prefectNine']);
+        $library_PrefectNineSQL = "SELECT COUNT(library_prefect) AS library_prefectNine FROM vote WHERE library_prefect = '$library_PrefectNine'";
+        $library_PrefectNineRESULT = mysqli_query($conn, $library_PrefectNineSQL);
+        while($library_PrefectNineROW = mysqli_fetch_array($library_PrefectNineRESULT)){
+            echo $library_PrefectNineROW['library_prefectNine'].' <h6>VOTE(S)</h6>';
+        }
+    }
+       //==============================| TEN library PREFECT
+    if(isset($_POST['library_prefectTen'])){
+        $library_PrefectTen = mysqli_real_escape_string($conn, $_POST['library_prefectTen']);
+        $library_PrefectTenSQL = "SELECT COUNT(library_prefect) AS library_prefectTen FROM vote WHERE library_prefect = '$library_PrefectTen'";
+        $library_PrefectTenRESULT = mysqli_query($conn, $library_PrefectTenSQL);
+        while($library_PrefectTenROW = mysqli_fetch_array($library_PrefectTenRESULT)){
+            echo $library_PrefectTenROW['library_prefectTen'].' <h6>VOTE(S)</h6>';
+        }
+    }
+
 ?>
